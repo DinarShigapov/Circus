a86fa98f896540fbb82886d6a642a543480f6142
FLAS SON <dinar-shigapov@inbox.ru>
2022-12-07 20:04:59 +0300

    f

--- Trainer/Model/CircusDB.Context.cs
+++ Trainer/Model/CircusDB.Context.cs
@@ -28,7 +28,6 @@ namespace Trainer.Model
         public virtual DbSet<Animal> Animal { get; set; }
         public virtual DbSet<AnimalType> AnimalType { get; set; }
         public virtual DbSet<Cage> Cage { get; set; }
-        public virtual DbSet<Client> Client { get; set; }
         public virtual DbSet<Employee> Employee { get; set; }
         public virtual DbSet<Item> Item { get; set; }
         public virtual DbSet<ItemEmployee> ItemEmployee { get; set; }
--- Trainer/Model/TicketSale.cs
+++ Trainer/Model/TicketSale.cs
@@ -17,8 +17,12 @@ namespace Trainer.Model
         public int Id { get; set; }
         public int PerformanceId { get; set; }
         public int ClientId { get; set; }
+        public string LastNameClient { get; set; }
+        public string FirstNameClient { get; set; }
+        public string PatronymicClient { get; set; }
+        public string PhoneClient { get; set; }
+        public int TicketAmount { get; set; }
     
-        public virtual Client Client { get; set; }
         public virtual Performance Performance { get; set; }
     }
 }
